e966bdf405f178154ed952f5ff75f9cdf5310429
Damir <damirzzz@yandex.ru>
2021-10-02 22:09:41 +0500

    Reafactoring of pogoda_age.py

--- pogoda_age.py
+++ pogoda_age.py
@@ -5,23 +5,19 @@ import os
 import serial
 import sys
 import datetime
-import pymysql
 from urllib.request import Request, urlopen
 import urllib
 import urllib.error
 import urllib.request
-#import smbus
 from dateutil.relativedelta import relativedelta
-
 
 
 dt = datetime.datetime.now()
 date1 = dt.day
 
-
 file = 'ya.html'
 
-
+# забираем данные с яндекса
 req0='https://yandex.ru/pogoda/ufa?lat=54.819809&lon=56.116405'
 req = Request(req0, headers={'User-Agent': 'Mozilla/5.0'} )
 try:
@@ -40,13 +36,12 @@ else:
 
 
 
-
+# парсинг файла и получение необходимых данных
 if os.path.exists(file):
     page = open(file, 'r', encoding="utf8")
     f = page.read()
 
-    #f_temp=re.search(r'fact__temp fact__temp_size_s" role="text"><span class="temp__value">(.*?)</span>', f)
-    #f_temp=re.search(r'Текущая температура</span><span class="temp__value">(.*?)</span>', f)
+    # температура
     f_temp=re.search(r'Текущая температура</span><span class="temp__value temp__value_with-unit">(.*?)</span>', f)
     if f_temp:
         f_temp=f_temp.group(1)
@@ -54,55 +49,41 @@ if os.path.exists(file):
         f_temp = f_temp.replace('+', '+')
         #print (f_temp)
 
-
-    #f_temp1=re.search(r'Ощущается как</div><div class="term__value"><div class="temp" role="text"><span class="temp__value">(.*?)</span>', f)
+    # температура, ощущается как
     f_temp1=re.search(r'Ощущается как</div><div class="term__value"><div class="temp" role="text"><span class="temp__value temp__value_with-unit">(.*?)</span>', f)
-    
-    
-    
     if f_temp1:
         f_temp1=f_temp1.group(1)
         f_temp1 = f_temp1.replace('−', '-')
         f_temp1 = f_temp1.replace('+', '+')
         #print (f_temp1)
 
-
+    # давление
     p=re.search(r'icon_pressure-white term__fact-icon" aria-hidden="true"></i>(.*?)<span', f)
     if p:
         p=p.group(1)
         #print (p)
 
-
+    # влажность
     h=re.search(r'icon icon_humidity-white term__fact-icon" aria-hidden="true"></i>(.*?)</div>', f)
     if h:
         h=h.group(1)
         #print (h)
 
+    # ветер
     w=re.search(r'wind-airflow-white term__fact-icon" aria-hidden="true"></i><span class="wind-speed">(.*?)</span>', f)
-    #w=re.search(r'term__value" aria-label="Ветер: (.*?) Метров в секунду', f)
-
     if w:
         w=w.group(1)
         #print (w)
 
-        #date1 = str(date1)
-        #usl = 'condition day-anchor i-bem" data-bem=\'{"day-anchor":{"anchor":'+date1+'}}\'>(.*?)</div>'
-
-
-
+        
+    # направление ветра
     wd=re.search(r'role="text">([А-Я,\-\s]{,5})</abbr><i class="icon icon_size_12 icon_wind', f)
     if wd:
         wd=wd.group(1)
         #print (p)
 
-
-    #cond=re.search(r'>(.*?)</div><div class="term term_orient_h fact__feels-like', f)
+    # облачность, осадки, ясно и т.д.
     cond=re.search(r'condition day-anchor i-bem" data-bem=\'{"day-anchor":{"anchor":\d{1,2}}}\'>(.*?)</div>', f)
-    #cond = re.search(r'}}\'>(.*?)</div><div class="term term_orient_h fact__feels-like', f)
-    #cond=re.search(r'usl', f)
-    #cond=re.search(r'}}\'>(.*?)</div>', f)
-
-        
     if cond:
         cond=cond.group(1)
         if cond == 'Ясно':
@@ -134,60 +115,44 @@ if os.path.exists(file):
         
 
         
-
+# вычисление возраста ребенка
 bd = datetime.datetime(2021, 2, 22)
 now = datetime.datetime.now()
 delta = relativedelta(now, bd)
-#(delta.months, '+', delta.days, sep='')
 
-
-
-
-#открываем порт
+# открываем порт Windows
 ser = serial.Serial('COM13', 9600, dsrdtr = 1,timeout = 0)
-#ser = serial.Serial('/dev/ttyACM0', 9600, dsrdtr = 1,timeout = 0)
+
+# работа с портом в Linux
+# ser = serial.Serial('/dev/ttyACM0', 9600, dsrdtr = 1,timeout = 0)
 
 
-#text1=f_temp+' a '+f_temp1+' P:'+p + str(proc)
+# на одну строку дисплея можно выводить 20 символов
+# собираем первую строку
 text1=f_temp+' a '+f_temp1+' P:'+p + str(delta.months) + '+' + str(delta.days)
-#+' as '+f_temp1+' P:'+p
-#text2='H: '+h+' W:'+w+'m/s'
-#text2= c +' W:' + str(w) + ' ' + str(week) + '+' + str(week_ost) + ' ' + str(ost.days)
+
+# собираем вторую строку
 text2= c +' W:' + str(w) + ' ' + str(dt.hour) + ':' + str(dt.minute)
-#str('{:.0f}'.format(h_dat))
-#text2= c
 
-
-
-
-
-
+# т.к. длина строки меняется в зависимости от данных, заполняем строку до 20 символов
 text1=text1.ljust(20, ' ')
 text2=text2.ljust(20, ' ')
 
+# итоговая строка
 text=text1+text2
 
 #if w != 'Штиль':
-w = w.replace(",", ".")
-w = float(w)
-
-#else:
- #   w = 0
-
-#h =  h[0 : -1]
+#w = w.replace(",", ".")
 #w = float(w)
 
-
-
-
-
+# вывод на экран компа при запуске
 print('--------------------------------------')
-#print (text + h + ' '+ wd)
 print (text)
 
+# задержка, чтобы успеть увидеть
 time.sleep(3)
 
-#text = '123456781234567812345678901234567890'
+# вывод на дисплей
 text = text.encode('cp866')
 ser.write(text)
 ser.close()
