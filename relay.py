4c53d1a364883739e2274a3b44a73a610c0f6890
Damir Galiev <damirzzz@yandex.ru>
2021-11-25 22:24:28 +0500

    Добавлен режим повтора включениявыключения

--- relay.py
+++ relay.py
@@ -24,7 +24,7 @@ st = {0: 1, 1: 0}  # словарь состояний реле
 
 if (len_arg == 2 and ((sys.argv[1] == '-h') or (sys.argv[1] == '--help') or (sys.argv[1] == '/?'))):
     print(
-        "Параметры для управления реле:\nимя_файла.py [номер розетки] [1/0 - вкл/выкл] [время работы реле в секундах]\nномер розетки и действие - обязательные параметры,\nвремя работы - необязательный параметр")
+        "Параметры для управления реле:\nимя_файла.py [номер розетки] [0/1/2 - выкл/вкл/повтор] [время работы реле в секундах]\nномер розетки и действие - обязательные параметры,\nвремя работы - необязательный параметр")
 
 elif (len_arg == 3):
     GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
@@ -32,10 +32,27 @@ elif (len_arg == 3):
     logs(sys.argv[1], sys.argv[2])
 
 elif (len_arg == 4):
-    GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
-    GPIO.output(pin[int(sys.argv[1])], 0)
-    time.sleep(int(sys.argv[3]))
-    GPIO.output(pin[int(sys.argv[1])], 1)
+    if sys.argv[2]==1:
+        GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
+        GPIO.output(pin[int(sys.argv[1])], 0)
+        time.sleep(int(sys.argv[3]))
+        GPIO.output(pin[int(sys.argv[1])], 1)
+                
+    if sys.argv[2] > 1:
+        i = 0
+        while i < int(sys.argv[2]):
+            print(i)
+            GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
+            GPIO.output(pin[int(sys.argv[1])], 0)
+            time.sleep(int(sys.argv[3]))
+            GPIO.output(pin[int(sys.argv[1])], 1)
+            i = i + 1
+            if i < int(sys.argv[2]):
+                time.sleep(int(sys.argv[3]))
+            
+            
+              
+        
 
 else:
     print('Error. Please, input right parameters')
