0d5dd51e90d05238c9c10d1c189d5375fca3d889
Damir Galiev <damirzzz@yandex.ru>
2022-01-17 00:10:33 +0500

    Добавлен 4-й параметр при запуске - время простоя

--- relay.py
+++ relay.py
@@ -32,27 +32,40 @@ elif (len_arg == 3):
     logs(sys.argv[1], sys.argv[2])
 
 elif (len_arg == 4):
-    if sys.argv[2]==1:
+    if sys.argv[2] == 1:
         GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
         GPIO.output(pin[int(sys.argv[1])], 0)
         time.sleep(int(sys.argv[3]))
         GPIO.output(pin[int(sys.argv[1])], 1)
-                
+        logs(sys.argv[1], sys.argv[2])
+
     if sys.argv[2] > 1:
         i = 0
         while i < int(sys.argv[2]):
-            print(i)
+            print(i+1)
             GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
             GPIO.output(pin[int(sys.argv[1])], 0)
             time.sleep(int(sys.argv[3]))
             GPIO.output(pin[int(sys.argv[1])], 1)
+            logs(sys.argv[1], sys.argv[2])
             i = i + 1
             if i < int(sys.argv[2]):
                 time.sleep(int(sys.argv[3]))
-            
-            
-              
-        
+
+elif (len_arg == 5):
+    if sys.argv[2] > 1:
+        i = 0
+        while i < int(sys.argv[2]):
+            print(i+1)
+            GPIO.setup(pin[int(sys.argv[1])], GPIO.OUT)
+            GPIO.output(pin[int(sys.argv[1])], 0)
+            time.sleep(int(sys.argv[3]))
+            GPIO.output(pin[int(sys.argv[1])], 1)
+            logs(sys.argv[1], sys.argv[2])
+            i = i + 1
+            if i < int(sys.argv[2]):
+                time.sleep(int(sys.argv[4]))
+
 
 else:
     print('Error. Please, input right parameters')
